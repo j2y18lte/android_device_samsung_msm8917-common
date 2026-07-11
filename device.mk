0ed173114d2ee4795ad7d49faad0dd6197cc4c4e
Ma3a_XaTa <v.radulevich@gmail.com>
2026-07-11 10:15:22 +0300

    j2y18lte: Initial Ramdisk

--- device.mk
+++ device.mk
@@ -169,6 +169,19 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     power.msm8937
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.qcom.rc \
+    init.qcom.sh \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    init.target.rc \
+    ueventd.qcom.rc \
+    init.qcom.bt.sh \
+    init.qcom.fm.sh \
+    init.qcom.post_boot.sh
+
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
