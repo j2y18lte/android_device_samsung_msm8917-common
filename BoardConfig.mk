d6fceb22e0bb588ac87da55d141c7d44d14837f6
Ma3a_XaTa <v.radulevich@gmail.com>
2026-07-11 10:01:41 +0300

    j2y18lte: build kernel from source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,16 @@ TARGET_CPU_VARIANT := cortex-a53
 TARGET_BOARD_PLATFORM := msm8937
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno308
 
+# Kernel
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
+BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_PAGESIZE :=  2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
+TARGET_KERNEL_CONFIG := j2y18lte_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8917
+
 # Audio
 AUDIO_FEATURE_ENABLED_ACDB_LICENSE := true
 AUDIO_FEATURE_ENABLED_ANC_HEADSET := true
