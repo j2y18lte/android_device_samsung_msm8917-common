8bd3173d6a419dbf43b9680c194eae27415f0b85
Ma3a_XaTa <v.radulevich@gmail.com>
2026-07-11 11:48:59 +0300

    j2y18lte: import data-ipa-cfg-mgr * CAF Tag LA.UM.5.6.r1-00500-89xx.0 * add makefile (a8ee84a6aa9c75301bad238595bf90df44c74f7c) * Cleanup makefiles (5ac9f7ba2123c51fa048ae84e3b8be4b29fbc595) * Cleanup Obsolete LOCAL_PRELINK_MODULE. (ad3b6a46b5c40a2c2a3bf4d0d49cbb35224b1e42) * Remove bogus dependency(7ecd02cf69c700d71e8ce72ed287209f45677e1d) * Remove verbose cflag (5193df3367c9f5ce81c445956556e072b8a4f32a) * Disable DEBUG for userdebug builds (48247faef80a705f3592742b9bf2571a585925a2)

--- device.mk
+++ device.mk
@@ -133,6 +133,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
+# IPA Manager
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
